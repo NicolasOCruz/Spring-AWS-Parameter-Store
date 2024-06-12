768314bfcf993e86394fb32eab001045e946a7be
NicolasOCruz <nicolaspereira244@gmail.com>
2024-06-11 23:33:29 -0300

    Recuperando parâmetros via @Value

---
+++ src/main/java/com/nicolascruz/parameter_store/ParameterStoreApplication.java
@@ -0,0 +1,13 @@
+package com.nicolascruz.parameter_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParameterStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParameterStoreApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nicolascruz/parameter_store/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.nicolascruz.parameter_store.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@RefreshScope
+public class TestController {
+
+    @Value("${process.content.url}")
+    private String processUrl;
+
+    @GetMapping
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok(processUrl);
+    }
+}
